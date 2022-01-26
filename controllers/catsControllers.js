1f73d4f3be69b9a9e41103ef5b90c84bdbcb9414
Akshit Kumar <akshitkumar450@gmail.com>
2022-01-27 02:25:53 +0530

    using data from db for request and response

--- controllers/catsControllers.js
+++ controllers/catsControllers.js
@@ -1,110 +1,78 @@
-const fs = require("fs");
-const path = require("path");
 const db = require("../db");
 
-// reading the json file
-let cats = JSON.parse(fs.readFileSync(path.join(__dirname, "../data.json")));
-// console.log(cats);
 const getAllCats = (req, res) => {
-  // let allCats = [];
   db.query("SELECT * FROM Cats ", (err, result, fields) => {
     if (err) throw err;
     console.log("all", result);
-    // allCats = result.map((item) => item);
-    // console.log(allCats);
-    res.status(200).json(result);
+    if (result.length !== 0) {
+      res.status(200).json(result);
+      return;
+    }
+    res.status(404).send("Not cats present");
   });
 };
 
 const createNewCat = (req, res) => {
   const { name, age, breed, image } = req.body;
-  cats = [...cats, { name, age, breed, image, id: Math.random() * 10 }];
-  //   console.log(cats);
-  fs.writeFile(
-    path.join(__dirname, "../data.json"),
-    JSON.stringify(cats),
-    (err) => {
-      console.log(err);
-    }
-  );
-  res.status(201).json(cats);
+  let sql = "INSERT INTO Cats (Name,Breed,Age,Image) VALUES ?";
+  const values = [[name, breed, age, image]];
+  db.query(sql, [values], (err, result) => {
+    if (err) throw err;
+    console.log("table updated");
+  });
+  getAllCats(req, res);
 };
 
 const deleteCatById = (req, res) => {
-  //   console.log(req.params.id);
-
-  //   find the cat with given id
-  let toBeDeleted = cats.find((cat) => cat.id === +req.params.id);
-  //   console.log(toBeDeleted);
-  if (!toBeDeleted) {
-    res.status(404).send("cat with given is id not there");
-    return;
-  }
-  //   remove the cat which we have find with the id from cats array
-  cats = cats.filter((cat) => cat !== toBeDeleted);
-  //   console.log(cats);
-  fs.writeFile(
-    path.join(__dirname, "../data.json"),
-    JSON.stringify(cats),
-    (err) => {
-      console.log(err);
-    }
-  );
-  res.status(204).json(null);
-};
-
-const updateCatById = (req, res) => {
-  //   console.log("put reques", req.params.id);
-  const toBeUpdated = cats.find((cat) => cat.id === +req.params.id);
-  console.log("before", toBeUpdated);
-  toBeUpdated.age = req.body.age;
-  toBeUpdated.name = req.body.name;
-  toBeUpdated.breed = req.body.breed;
-  toBeUpdated.image = req.body.image;
-  console.log("after", toBeUpdated);
-
-  cats = [...cats, toBeUpdated];
-  console.log("final", cats);
-
-  //   fs.writeFile(
-  //     path.join(__dirname, "../data.json"),
-  //     JSON.stringify(cats),
-  //     (err) => {
-  //       console.log(err);
-  //     }
-  //   );
-  res.send("put");
+  db.query(`DELETE FROM Cats where id=${req.params.id}`, (err, result) => {
+    if (err) throw err;
+    console.log("deleted");
+    res.status(204).json(null);
+  });
 };
 
 const findByName = (req, res) => {
-  const data = cats.filter((cat) => cat.name === req.params.name)[0];
-  if (data) {
-    const { age, breed, name, image, id } = data;
-    res.status(200).json({
-      name,
-      age,
-      breed,
-      image,
-      id,
-    });
-    return;
-  }
-  res.status(404).send("Not found 💩💩");
+  const name = req.params.name;
+  db.query(`SELECT * FROM Cats WHERE Name='${name}'`, (err, result) => {
+    if (err) throw err;
+    if (result.length !== 0) {
+      res.status(200).json(result[0]);
+      return;
+    }
+    res.status(404).send("Not found 💩💩");
+  });
+};
+
+const updateCatById = (req, res) => {
+  console.log("put reques", +req.params.id);
+  db.query(
+    `UPDATE Cats SET Name='${req.body.name}',Age=${req.body.age},Breed='${
+      req.body.breed
+    }',Image='${req.body.image}' WHERE id=${+req.params.id}`,
+    (err, result) => {
+      if (err) throw err;
+      res.status(200).json({
+        status: "updated",
+      });
+    }
+  );
 };
 
 const findByAge = (req, res) => {
-  //   console.log("find by age");
-  //   console.log(req.query.age_lte, req.query.age_gte, req.url);
-  if (req?.query?.age_lte && req?.query?.age_gte) {
-    cats = cats.filter(
-      (cat) => cat.age > req.query.age_lte && cat.age < req.query.age_gte
-    );
-    if (!cats) {
-      res.status(404).send("Not found between age 💩💩");
-      return;
+  // console.log(+req.query.age_lte, +req.query.age_gte, req.url);
+  db.query(
+    `SELECT * FROM Cats WHERE Age BETWEEN ${+req.query.age_lte} AND ${+req.query
+      .age_gte}`,
+    (err, result) => {
+      if (err) throw err;
+      if (result.length !== 0) {
+        console.log(result);
+        res.status(200).json(result);
+        return;
+      }
+      res.status(404).send("Not cat between the age");
     }
-    res.status(200).json(cats);
-  }
+  );
 };
 
 // for checking whether we have a data in post request
